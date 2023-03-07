4f6d1a935cce6be9bffb79f48384a6c75cd1d622
Veronika Popova <freebird130188@gmail.com>
2023-03-07 20:22:11 +0100

    Added file index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  let age = Number(data["age"]);
+  let badness = Number(data["badness"]);
+  let ageBadnessSum = age + badness;
+  console.log(ageBadnessSum);
+});
